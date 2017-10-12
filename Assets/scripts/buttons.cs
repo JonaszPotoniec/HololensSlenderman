2777a1525585ff754424cf78e770608ccfa50596
praktykant2 <praktykant2@CARBON.CTI.P.LODZ.PL>
2017-10-12 14:04:06 +0200

    small changes in menu

---
+++ Assets/scripts/buttons.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttons : MonoBehaviour {
+
+    public bool isClickable = true;
+    public string functionName;
+    public string functionArguments;
+    public Material[] materials;
+
+
+    private bool state;
+    private GameObject settingsOBJ;
+
+	// Use this for initialization
+	void Start () {
+        
+        settingsOBJ = GameObject.Find("settings");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void onSelect()
+    {
+        if (isClickable)
+        {
+            state = !state;
+
+            if(state)
+                transform.GetComponent<MeshRenderer>().sharedMaterial = materials[1];
+            else
+                transform.GetComponent<MeshRenderer>().sharedMaterial = materials[0];
+        }
+
+        settingsOBJ.SendMessageUpwards(functionName, functionArguments);
+    }
+}
